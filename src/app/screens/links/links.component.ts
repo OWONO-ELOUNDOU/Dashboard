5471c67a52c7463abeb683ff6f3bee9ecb13a249
OWONO ELOUNDOU <OWONO ELOUNDOU>
2023-04-25 23:39:24 +0100

    link changed

---
+++ src/app/screens/links/links.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { map } from 'rxjs';
+import { ServicesService } from 'src/app/Services/services.service';
+
+@Component({
+  selector: 'app-links',
+  templateUrl: './links.component.html',
+  styleUrls: ['./links.component.css']
+})
+export class LinksComponent {
+  Links: any[] = [];
+
+
+  constructor(
+    private apiService: ServicesService
+  ) { }
+
+  ngOnInit(): void{
+    this.apiService.getLinks().pipe(map((res) => {
+      const links = [];
+      for(const key in res){
+        if(res.hasOwnProperty(key)){
+          links.push({...res[key], id: key})
+        }
+      }
+      return links;
+    }))
+    .subscribe((links) => {
+      console.log(links);
+      this.Links = links;
+    })
+    //console.log(this.Links);
+    console.log(this.linkCount())
+  }
+
+  linkCount() {
+    let nbLink = this.Links.length;
+    localStorage.setItem('nbLink',JSON.stringify(nbLink));
+    return this.Links.length;
+  }
+
+}
